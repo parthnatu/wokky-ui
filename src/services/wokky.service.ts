0675731ed6180da256aec07c9e77d06dab428f31
Parth Natu <parth.natu@gmail.com>
2024-10-22 16:50:20 -0500

    feat: added Dockerfile

--- src/services/wokky.service.ts
+++ src/services/wokky.service.ts
@@ -1,8 +1,14 @@
 import http from "../http-common";
 import WokkyDTO from "../models/wokky";
-
+import axiosRetry from "axios-retry";
+axiosRetry(http, {
+  retries: 3, // Number of retries (Defaults to 3)
+});
 const isWokky = (_latitude: number, _longitude: number) => {
-  return http.post<WokkyDTO>("/wokky_time_now", {latitude : _latitude, longitude: _longitude});
+  return http.post<WokkyDTO>("/wokky_time_now", {
+    latitude: _latitude,
+    longitude: _longitude,
+  });
 };
 const WokkyService = {
   isWokky,
